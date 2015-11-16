83dd123a0df78a013db66cc88f0318004def69e3
Patrick Boehner <patrick.boehner@gmail.com>
2015-11-16 01:52:39 -0800

    Forgot a few files.

---
+++ front-page.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This is the homepage template file.
+ *
+ * @author Patrick Boehner
+ * @package  [Blank Genesis Child Theme]
+ *
+ */
+
+//* Force full width layout
+add_filter( 'genesis_pre_get_option_site_layout', '__genesis_return_full_width_content' );
+
+//* Run the Genesis loop
+genesis();
