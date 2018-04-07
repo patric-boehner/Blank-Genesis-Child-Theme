fe11a6c53886e07077a9e21e981af895cc9f1a90
Patrick Boehner <patrick@patrickboehner.com>
2018-04-07 12:19:04 -0700

    Up version number

--- functions.php
+++ functions.php
@@ -16,7 +16,7 @@ if( !defined( 'ABSPATH' ) ) exit;
 // Child theme (do not remove).
 define( 'CHILD_THEME_NAME', 'Blank Child Theme' );
 define( 'CHILD_THEME_URL', 'http://example.com/' );
-define( 'CHILD_THEME_VERSION', '1.0.7' );
+define( 'CHILD_THEME_VERSION', '2.0.8' );
 
 
 // Cache Busting
