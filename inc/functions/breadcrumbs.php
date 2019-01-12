6491ab2f8cfc0b4c1a0aab24817cc2de9ff743ca
Patrick <patrick@patrickboehner.com>
2019-01-12 11:20:24 -0800

    Clenaup

--- inc/functions/breadcrumbs.php
+++ inc/functions/breadcrumbs.php
@@ -21,9 +21,12 @@ add_filter( 'genesis_archive_crumb', 'pb_add_blog_crumb', 10, 2 );
 function pb_add_blog_crumb( $crumb, $args ) {
 
 	if ( is_singular( 'post' ) || is_category() ) {
+
 		return '<a href="' . get_permalink( get_option( 'page_for_posts' ) ) . '">' . get_the_title( get_option( 'page_for_posts' ) ) .'</a> ' . $args['sep'] . ' ' . $crumb;
 	} else {
+
 		return $crumb;
+
 	}
 
 }
