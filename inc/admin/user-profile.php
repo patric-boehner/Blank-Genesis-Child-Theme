355ffa9820eff6bb48a459076dfbb4c4ca38cc91
Patrick <patrick@patrickboehner.com>
2019-01-12 11:32:05 -0800

    Add user contact methods

---
+++ inc/admin/user-profile.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Add User contact Methods
+ *
+ * @package Blank Child Theme
+ * @author  Patrick Boehner
+ * @license GPL-2.0+
+ * @link    http://example.com/
+ */
+
+
+// If this file is called directly, abort.
+//**********************
+if( !defined( 'ABSPATH' ) ) exit;
+
+
+// Register User Contact Methods
+add_filter( 'user_contactmethods', 'custom_user_contact_methods' );
+function custom_user_contact_methods( $user_contact_method ) {
+
+	$user_contact_method['facebook'] = __( 'Facebook', 'blank-child-theme' );
+	$user_contact_method['twitter'] = __( 'Twitter', 'blank-child-theme' );
+	$user_contact_method['linkedin'] = __( 'LinkedIn', 'blank-child-theme' );
+
+	return $user_contact_method;
+
+}
